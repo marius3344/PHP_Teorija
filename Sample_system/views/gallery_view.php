fba7d50a3fdfe2160922a0e0499cc26d07d097a7
marius.simkunas.flgroup <marius3344@gmail.com>
2022-03-10 19:28:50 +0200

    Update delete

--- Sample_system/views/gallery_view.php
+++ Sample_system/views/gallery_view.php
@@ -1,8 +1,8 @@
 <?php 
-session_start();
-if(!isset($_SESSION['username'])){
-    header("Location: login.php");
-}
+//session_start();
+// if(!isset($_SESSION['username'])){
+//     header("Location: login.php");
+// }
 include '../layout/header.php';
 
 
--- Sample_system/views/users.php
+++ Sample_system/views/users.php
@@ -19,7 +19,7 @@ try{
 ?>
 <div class="container py-4">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-12">
             <div class="card text-center">
                 <div class="card-header">
                     Hello, <?php echo $_SESSION['username']; ?>
@@ -33,10 +33,11 @@ try{
                             <th>Email</th>
                             <th>Created</th>
                             <th>Updated</th>
+                            <th>Action</th>
                         </tr>
                         <?php
                         foreach($result as $user){
-                            echo "<tr><td>".$user['first_name']."</td><td>".$user['last_name']."</td><td>".$user['email']."</td><td>".$user['created']."</td><td>".$user['updated']."</td></tr>";
+                            echo "<tr><td>".$user['first_name']."</td><td>".$user['last_name']."</td><td>".$user['email']."</td><td>".$user['created']."</td><td>".$user['updated']."</td><td><a class='btn btn-warning' href='user_edit.php?userid=".$user['id']."'>Edit</a><a class='btn btn-danger' href='../scripts/user_delete.php?userid=".$user['id']."'>Delete</a></td></tr>";
                         }
                         ?>
                     </table>
