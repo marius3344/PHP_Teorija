fb4bfc255ce097c8e50e0c85d3a5da220cb882a9
marius.simkunas.flgroup <marius3344@gmail.com>
2022-03-31 19:01:56 +0300

    this and access

--- OOP/index.php
+++ OOP/index.php
@@ -10,22 +10,42 @@
 //Klase - Class. Object is an instance of a Class
 
 
+//Access Modifiers
+// public, private, (protected)
+
+//public - metodus ir savybes galime pasiekti ir klaseje ir klases isoreje
+
+//private - metodus ir savybes galime pasiekti tik klases viduje
+
+
 
 class BankAccount
 {
    public $accountNumber;
    public $balance;
+   private $user;
+
+   public function setUser($user){
+      if($user !== ""){
+         $this->user = $user;
+      }
+   }
+
+   public function getUser(){
+      return $this->user;
+   }
 
    public function deposit($amount){
       if($amount>0){
          $this->balance += $amount;
       }
+      return $this;
    }
 
    public function withdraw($amount){
       if($amount <= $this->balance){
          $this->balance -= $amount;
-         return true;
+         return $this;
       }
       return false;
    }
@@ -38,12 +58,27 @@ $myAccount->balance = 100;
 
 $myAccount->deposit(50);
 
-echo $myAccount->balance;
+//echo $myAccount->balance;
 //echo $myAccount->accountNumber;
+
+//$myAccount->user = "Jonas"; neveikia, nes private
+
+$myAccount->setUser("Jonas");
+echo $myAccount->getUser();
+
+
+
+
+
 
 $yourAccount = new BankAccount;
 $yourAccount->accountNumber = 32156;
 $yourAccount->balance = 1000;
+
+$yourAccount->deposit(100);
+$yourAccount->deposit(100);
+
+$yourAccount->deposit(100)->deposit(100)->withdraw(500);
 
 //var_dump($yourAccount);
 
