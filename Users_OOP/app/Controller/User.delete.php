152c6cf28b564e817420ac93d196fa7e00d2ca3d
marius.simkunas.flgroup <marius3344@gmail.com>
2022-04-12 21:38:26 +0300

    OOP CRUD2

---
+++ Users_OOP/app/Controller/User.delete.php
@@ -0,0 +1,18 @@
+<?php 
+
+$id = isset($_GET['id']) ? $_GET['id'] : die("ERROR: no User ID");
+
+require "../../config/Database.php";
+require "User.php";
+
+$database = new Database;
+$db = $database->getConnection();
+
+$user = new User($db);
+$user->id = $id;
+
+if ($user->delete()){
+    header ("Location: ../../../");
+} else {
+    echo "delete failed";
+}
--- Users_OOP/index.php
+++ Users_OOP/index.php
@@ -1,20 +1,31 @@
 <?php 
-require_once "config/Database.php";
-
-$db = new Database;
-$db->getConnection();
-
-
-
 $title = "Home";
-
 include "layout/header.php";
+
+require_once "config/Database.php";
+require "app/Controller/User.php";
+
+$database = new Database;
+$db = $database->getConnection();
+
+$users = User::index($db);
 
 ?>
 <ul>
     <li><a href="views/user.create.php">User Create</a></li>
 </ul>
-
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Actions</th>
+    </tr>
+    <?php
+    foreach($users as $user){
+        echo "<tr><td>".$user['name']."</td><td>".$user['email']."</td><td><a href='views/user.edit.php/?id=".$user['id']."'>Update</a><a href='app/Controller/user.delete.php/?id=".$user['id']."'>Delete</a></td></tr>";
+    }
+    ?>
+</table>
 
 <?php 
 include "layout/footer.php";
